641ac2a3e7345e93851b518e762aefb375ada2be
juwoon01 <juwoon01@naver.com>
2021-01-04 01:25:37 +0900

    Add Navigation

---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function About(){
+    return <span>새로고침 없이 페이지를 이동하기 위해서는 react-router-dom의 Link를 사용해야하며 Link를 사용하기 위해서는 Route를 사용해야 한다.</span>;
+
+}
+
+export default About;
